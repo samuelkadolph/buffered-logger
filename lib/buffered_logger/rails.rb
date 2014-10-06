25c3a8687a44ec679978738031ae21800f96b207
Israël Hallé <israel.halle@shopify.com>
2014-10-06 18:18:07 +0000

    Support RailsStdoutLogging

--- lib/buffered_logger/rails.rb
+++ lib/buffered_logger/rails.rb
@@ -6,17 +6,22 @@ class BufferedLogger
     initializer :buffered_logger, :before => :initialize_logger do |app|
       next if app.config.logger
 
-      if Rails::VERSION::STRING >= "3.1"
-        path = app.paths["log"].first
+      logdev = if defined? RailsStdoutLogging
+        STDOUT
       else
-        path = app.paths.log.to_a.first
+        if Rails::VERSION::STRING >= "3.1"
+          path = app.paths["log"].first
+        else
+          path = app.paths.log.to_a.first
+        end
+
+        file = File.open(path, "a")
+        file.binmode
+        file.sync = true
+        file
       end
 
-      file = File.open(path, "a")
-      file.binmode
-      file.sync = true
-
-      app.config.logger = BufferedLogger.new(file)
+      app.config.logger = BufferedLogger.new(logdev)
       app.config.logger.level = BufferedLogger.const_get(app.config.log_level.to_s.upcase)
       app.config.middleware.insert(0, BufferedLogger::Middleware, app.config.logger)
     end
