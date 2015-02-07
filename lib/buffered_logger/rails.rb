938947f8d311b32018e959fdf764d505ac97f74a
Peter McCracken <peter.mccracken@shopify.com>
2015-02-07 02:58:11 +0000

    add configuration option

--- lib/buffered_logger/rails.rb
+++ lib/buffered_logger/rails.rb
@@ -3,22 +3,29 @@ require "rails"
 
 class BufferedLogger
   class Railtie < Rails::Railtie
+    config.buffered_logger = ActiveSupport::OrderedOptions.new
+
     initializer :buffered_logger, :before => :initialize_logger do |app|
       next if app.config.logger
 
+      output_stream = app.config.buffered_logger.output_stream || default_output_stream(app)
+
+      output_stream.binmode
+      output_stream.sync = true
+
+      app.config.logger = BufferedLogger.new(output_stream)
+      app.config.logger.level = BufferedLogger.const_get(app.config.log_level.to_s.upcase)
+      app.config.middleware.insert(0, BufferedLogger::Middleware, app.config.logger)
+    end
+
+    def default_output_stream(app)
       if Rails::VERSION::STRING >= "3.1"
         path = app.paths["log"].first
       else
         path = app.paths.log.to_a.first
       end
 
-      file = File.open(path, "a")
-      file.binmode
-      file.sync = true
-
-      app.config.logger = BufferedLogger.new(file)
-      app.config.logger.level = BufferedLogger.const_get(app.config.log_level.to_s.upcase)
-      app.config.middleware.insert(0, BufferedLogger::Middleware, app.config.logger)
+      File.open(path, "a")
     end
   end
 end
