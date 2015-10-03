8a8ca6b2db1927443b09a9ad4c8f7e4d0217fabf
James Reid-Smith <james.reidsmith@shopify.com>
2015-10-03 09:10:55 -0400

    This test was able to exercise the thread safety issue with flush 10 out of the 10 times I tried it locally

--- test/buffered_logger/log_device_proxy_test.rb
+++ test/buffered_logger/log_device_proxy_test.rb
@@ -85,4 +85,24 @@ describe BufferedLogger::LogDeviceProxy do
       assert !@proxy.started?, "Proxy is started"
     end
   end
+
+  describe "flush" do
+    it "should handle multiple thread simutaneously calling it" do
+      @logdev.stubs(:write)
+
+      8.times do |iteration|
+        threads = []
+        8.times do
+          threads << Thread.new do
+            (iteration * 10_000).times do
+              @proxy.start
+              @proxy.flush
+              @proxy.end
+            end
+          end
+        end
+        threads.each { |thr| thr.join }
+      end
+    end
+  end
 end
