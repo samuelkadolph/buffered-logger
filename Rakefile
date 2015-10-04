89a1d5377ebda7a2af4ea2a3fbad6389ffee9c54
James Reid-Smith <james.reidsmith@shopify.com>
2015-10-04 09:20:30 -0400

    Was finding the new tests that brute force tested for thread safety issues too slow, so I've separated them from the other tests and added a new rake task (slow_test) to include them.

--- Rakefile
+++ Rakefile
@@ -5,6 +5,13 @@ require "rake/testtask"
 
 Rake::TestTask.new do |task|
   task.libs << "test"
+  task.test_files = Dir["test/**/*_test.rb"] - Dir["test/**/*_slow_test.rb"]
+  task.verbose = true
+end
+
+Rake::TestTask.new do |task|
+  task.name = :slow_test
+  task.libs << "test"
   task.test_files = Dir["test/**/*_test.rb"]
   task.verbose = true
 end
--- test/buffered_logger/log_device_proxy_test.rb
+++ test/buffered_logger/log_device_proxy_test.rb
@@ -58,24 +58,4 @@ describe BufferedLogger::LogDeviceProxy do
     @proxy.write("2")
     assert_equal "12", @proxy.current_log
   end
-
-  describe "flush" do
-    it "should handle multiple thread simutaneously calling it" do
-      @logdev.stubs(:write)
-
-      8.times do |iteration|
-        threads = []
-        8.times do
-          threads << Thread.new do
-            (iteration * 10_000).times do
-              @proxy.start
-              @proxy.flush
-              @proxy.end
-            end
-          end
-        end
-        threads.each { |thr| thr.join }
-      end
-    end
-  end
 end
